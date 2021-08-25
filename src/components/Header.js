717064e4662ce4e7a1ec81ce360bdda18e5b421c
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 16:50:43 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ export default class Header extends React.Component {
                   </div>
                 </div>
                 </React.Fragment>)}
-              </nav>
+              </nav>*/
             </header>
         );
     }
