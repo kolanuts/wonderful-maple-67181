dd1815f149d4e525fe6b3376af2bb781203fdaa4
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 16:35:46 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -40,7 +40,7 @@ export default class Body extends React.Component {
                     {(font !== 'system-sans') && (
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     )}
-                    {(font !== 'literata') && (
+                    {(font !== 'literata') ? (
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     )}
                     {(font === 'fraunces') ? (
